9d5896d3ba3042abe9c1b6d376dfa6706f1fcbe8
fabioNog <fabio.carmo@computacao.ufla.br>
2020-07-13 23:38:43 -0300

    Configurando Laravel com Angular

---
+++ resources/views/index.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+
+
+    </head>
+    <body>
+        <app-root></app-root>
+        <script src="{{ asset('js/runtime.js')}}" defer></script>
+        <script src="{{ asset('js/polyfills.js')}}" defer></script>
+        <script src="{{ asset('js/styles.js')}}" defer></script>
+        <script src="{{ asset('js/vendor.js')}}" defer></script>
+        <script src="{{ asset('js/main.js')}}" defer></script>
+    </body>
+</html>
